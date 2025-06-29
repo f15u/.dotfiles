fdbe2d1cee8a08d15d3f8ca70a362f926c1cf2a9
Federico Biccheddu <service+git@f15u.dev>
2025-06-29 19:27:27 +0200

    chore: add initial configuration

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "NixOS system configuration";
+
+  inputs = {
+    # Specify the nixpkgs channel you want to use
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    
+    # Optional: home-manager if you want to manage user configurations
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+        }
+      ];
+    };
+  };
+}
