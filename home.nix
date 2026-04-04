c0906885bf65555d3cd4ef69f796a9ec4c317f94
Federico Biccheddu <service+git@f15u.dev>
2026-04-04 13:49:55 +0200

    chore: improve librewolf setup

--- home.nix
+++ home.nix
@@ -32,4 +32,16 @@
 
     stateVersion = "25.05";
   };
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "librewolf.desktop";
+      "x-scheme-handler/http" = "librewolf.desktop";
+      "x-scheme-handler/https" = "librewolf.desktop";
+      "x-scheme-handler/about" = "librewolf.desktop";
+      "x-scheme-handler/unknown" = "librewolf.desktop";
+      "x-scheme-handler/discord" = "vesktop.desktop";
+    };
+  };
 }
